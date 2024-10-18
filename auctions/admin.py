0389423ff8a567f954e58fd06a509feab0820fb6
aegis945 <artiom.lekincev@gmail.com>
2024-10-18 18:34:13 +0300

    add models to the admin panel

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import AuctionListing, Bid, Category, Comment, User
 
-# Register your models here.
+admin.site.register(AuctionListing)
+admin.site.register(Bid)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(User)
