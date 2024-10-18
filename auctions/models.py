3546c0233064b01c922557d6dc0d6e683bd61540
aegis945 <artiom.lekincev@gmail.com>
2024-10-18 18:33:52 +0300

    add user, category, listing and bid models

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,46 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
+# inherits from django's abstract user
 class User(AbstractUser):
     pass
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.category_name
+    
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class AuctionListing(models.Model):
+    item_name = models.CharField(max_length=50)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    image_url = models.URLField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    current_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+
+    def __str__(self) -> str:
+        return self.item_name
+
+
+class Bid(models.Model):
+    auction_listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="bids")
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name="bids")
+    bid_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    auction_listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name="comments")
+    comment_text = models.CharField(max_length=250)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
