47e06e2ac921f5b64796f21716a2f925d4d6ecf5
aegis945 <artiom.lekincev@gmail.com>
2024-10-21 17:37:23 +0300

    add placeholders to the form

--- auctions/forms.py
+++ auctions/forms.py
@@ -7,11 +7,25 @@ class AuctionListingForm(forms.ModelForm):
         fields = ["item_name", "description", "category", "image_url", "starting_bid"]
 
         widgets = {
-            'item_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'description': forms.Textarea(attrs={'rows': 4, 'class': 'form-control'}),
-            'category': forms.Select(attrs={'class': 'form-control'}),
-            'image_url': forms.URLInput(attrs={'class': 'form-control'}),
-            'starting_bid': forms.NumberInput(attrs={'class': 'form-control'}),
+            'item_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': "Enter the title of your item"
+            }),
+            'description': forms.Textarea(attrs={
+                'rows': 4, 'class': 'form-control',
+                'placeholder': "Enter a detailed description"
+            }),
+            'category': forms.Select(attrs={
+                'class': 'form-control'
+            }),
+            'image_url': forms.URLInput(attrs={
+                'class': 'form-control',
+                'placeholder': "Add an image URL"
+            }),
+            'starting_bid': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': "Enter the starting bid amount",
+            }),
         }
 
         labels = {
